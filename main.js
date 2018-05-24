72b8e3dacaf00ac331e3f9dde3c5ceadcc3f73b6
Sathvik Reddy <sathvik.reddy615@gmail.com>
2018-05-24 14:43:59 -0500

    completed 'Conjuction Function'

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-let cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
-let y = 1
+// let cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+// let y = 1
 
-for (let x = 1; x < cookies.length; x++) {
-    let currentCookie = cookies[x]
-    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+// for (let x = 1; x < cookies.length; x++) {
+//     let currentCookie = cookies[x]
+//     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+// }
+
+const conjunction = (firstWord, secondWord) => {
+    const conjoinedWord = `${firstWord} ${secondWord}`;
+    console.log(conjoinedWord);
 }
+
+conjunction("Master", "Card");
