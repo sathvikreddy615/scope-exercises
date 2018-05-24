d824a4e19e39f775e6da220621d71eaff7b73d2e
Sathvik Reddy <sathvik.reddy615@gmail.com>
2018-05-24 15:17:21 -0500

    completed 'Lambda Llama' challenge

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]];
+const llamaNamer = () => {
+    const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"];
+    const randomizer = Math.floor(Math.random() * 7);
 
-for (let k = 0; k < locations.length; k++) {
-    const currentLocation = locations[k]
-
-    if (currentLocation[0] > 2) {
-        const invalidLocation = true;
+    const namer = () => {
+        const suffix = " the Llama";
+        const name = possibleNames[randomizer];
+        return [name, suffix]
     }
-
-    if (invalidLocation = false) {
-        console.log("This location is invalid");
-    }
-    console.log(`There were ${k} locations displayed`);
 }
+
+console.log(llamaNamer());
