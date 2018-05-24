fb11fd9066b2434c21b91530227173986d194280
Sathvik Reddy <sathvik.reddy615@gmail.com>
2018-05-24 14:27:26 -0500

    Completed the 'C is for Cookie' challenge

---
+++ main.js
@@ -0,0 +1,7 @@
+let cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+let y = 1
+
+for (let x = 1; x < cookies.length; x++) {
+    let currentCookie = cookies[x]
+    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+}
